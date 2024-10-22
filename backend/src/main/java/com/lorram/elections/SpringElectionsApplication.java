2c47c72e4a14f920405c1101fb978a8a968ea8a5
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-10-22 16:46:13 -0300

    Project created

---
+++ backend/src/main/java/com/lorram/elections/SpringElectionsApplication.java
@@ -0,0 +1,13 @@
+package com.lorram.elections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElectionsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lorram/elections/SpringElectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorram.elections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
